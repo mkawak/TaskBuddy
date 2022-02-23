dae334d791f73b1c4494a23d94a62e687e6588ad
allisonchuu <allisonlychu@gmail.com>
2022-02-23 14:46:58 -0800

    Added files for main and menu: Still in progress

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "UserAccount.hpp"
+#include <iostream>
+
+int main () {
+
+
+
+
+
+
+    return 0;
+
+}
---
+++ menu.hpp
@@ -0,0 +1,29 @@
+#ifndef MENU_H
+#define MENU_H
+
+#include <string>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Menu {
+	public:
+	void  getMainMenuInput();
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
+
+#endif

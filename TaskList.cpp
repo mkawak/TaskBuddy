300df0c86eb3b8f0832af0b2142d2e4f6c261c93
lightsofpeace <sjuan54145@gmail.com>
2022-03-02 14:08:57 -0800

    Implemented TaskList, Awaiting review

--- TaskList.cpp
+++ TaskList.cpp
@@ -5,19 +5,23 @@
 
 using namespace std;
 
-std::string getListName(){
-}
-std::string getCategory(){
-}
-int counter(){
-}
-void countUp(){
-}
-void addTask(){
-}
-void coundDown(){
-}
-void deleteTask(){
+string getListName(){return this->listName;}
+string getCategory(){return this->category;}
+int counter(){return this->list.size();}
+void countUp(){this->counter = this->counter + 1;}
+void addTask(task t){list.push_back(t);}
+void coundDown(){this->counter = this->counter - 1;}
+void deleteTask(task t){
+	for(int i = 0; i < this->list.size()-1; ++i){
+		if(this->list.at(i) == t){
+			list.erase(list.begin()+(i-1));
+		}
+	}
 }
 void displayList(){
+	cout << getListName() << ", " << getListCategory() << ":" << endl;
+	for(int i = 0; i < this->list.size()-1; ++i){
+		cout << this->list.at(i) << endl;
+	}
+
 }
